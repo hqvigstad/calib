116c24be3765a231f558890f338eaa4153c36c9e
Henrik Qvigstad <henrik.qvigstad@cern.ch>
2011-11-30 14:23:44 +0100

    Added test of ROOT inclusion.

---
+++ test/test_root.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <TTree.h>
+#include <TH1.h>
+#include <TCanvas.h>
+
+int main(int argc, char **argv) {
+    std::cout << "Test of inclusion of root into build process!"
+              << "A pdf file, hist.pdf, should be created in cd." << std::endl;
+
+    // test the inclusion of root to the build process.
+    TH1F* hist = new TH1F("hist", "histogram", 10, 0, 10);
+    TCanvas* canv = new TCanvas;
+    hist->Draw();
+    canv->SaveAs("hist.pdf");
+
+
+    return 0;
+}
